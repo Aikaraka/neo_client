41128251a9b61a9627642c912e7986758a1bc16c
hiimkwak <mindol0320@naver.com>
2024-11-23 17:01:54 +0900

    refactor: 파일 위치 이동, Navbar 추상화

---
+++ public/navbar/index.ts
@@ -0,0 +1,8 @@
+export { default as BoxFilled } from "./box-filled.svg";
+export { default as HomeFilled } from "./home-filled.svg";
+export { default as PencilFilled } from "./pencil-filled.svg";
+export { default as UserFilled } from "./user-filled.svg";
+export { default as BoxLine } from "./box-line.svg";
+export { default as HomeLine } from "./home-line.svg";
+export { default as PencilLine } from "./pencil-line.svg";
+export { default as UserLine } from "./user-line.svg";
