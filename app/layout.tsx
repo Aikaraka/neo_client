23524779875874c9b18989dc0184662f0e41e6f1
hiimkwak <mindol0320@naver.com>
2024-11-28 22:37:48 +0900

    style: 소설 상세 페이지

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const NanumSquareNeo = localFont({
+  src: "./fonts/NanumSquareNeo-Variable.woff2",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "NEO",
@@ -12,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={NanumSquareNeo.className}>
       <body className="max-w-md mx-auto">{children}</body>
     </html>
   );
