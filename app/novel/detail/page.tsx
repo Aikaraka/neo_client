b8b7cd48272f3d43b2d92f5c43545e967d0c0c18
hiimkwak <mindol0320@naver.com>
2024-11-28 22:54:09 +0900

    fix: 챗 페이지 중간 수정

--- app/novel/detail/page.tsx
+++ app/novel/detail/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/layout/scroll-area";
 import Image from "next/image";
 import Link from "next/link";
