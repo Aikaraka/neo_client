015c22c694fb79914805fa4ba60a2815b78788fb
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-27 23:22:04 +0900

    feat : 아이콘 있는 input 컴포넌트 추가

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,7 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { useFormContext } from "react-hook-form";
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
@@ -14,9 +15,39 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+type TextInputWithIconProps = React.ComponentProps<"input"> & {
+  IconComponent: React.ReactNode;
+};
+
+const TextInputWithIcon = React.forwardRef<
+  HTMLInputElement,
+  TextInputWithIconProps
+>(({ className, IconComponent, ...props }, ref) => {
+  const { register } = useFormContext();
+  return (
+    <div
+      className={cn(
+        "flex h-10 w-full items-center rounded-md border border-input bg-background px-3 py-2 md:text-sm ",
+        className
+      )}
+    >
+      <input
+        type="text"
+        className="flex-1 inset-1 bg-transparent focus-visible:ring-ring focus-visible:ring-offset-2 ring-offset-background focus-visible:outline-none disabled:opacity-50 disabled:cursor-not-allowed text-base"
+        ref={ref}
+        {...props}
+      />
+      {IconComponent && (
+        <span className="ml-4 w-6 text-muted-foreground">{IconComponent}</span>
+      )}
+    </div>
+  );
+});
+TextInputWithIcon.displayName = "TextInputWithIcon";
+
+export { TextInputWithIcon, Input };
