16e78612bcd8e35f05ac4be13363dd16a949e0d1
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-29 18:40:44 +0900

    feat : 404 페이지 추가

---
+++ app/[...404]/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Frown } from "lucide-react";
+
+export default function NotFound() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center flex-col gap-10 px-8">
+      <Frown className="text-primary" size={100} />
+      <div className="flex flex-col gap-3 justify-center itmes-center text-center">
+        <h1 className="text-5xl text-center">OOPS!</h1>
+        <h2>문제가 발생했어요</h2>
+      </div>
+      <a href="/" className="w-full">
+        <Button className="w-full">홈으로</Button>
+      </a>
+    </div>
+  );
+}
