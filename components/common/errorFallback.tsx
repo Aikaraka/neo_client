8cdf806937e433f5ccfbe76bc8cbd94988b75b48
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-29 18:46:57 +0900

    feat : ErrorBoundary Fallback ui 추가

---
+++ components/common/errorFallback.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { FallbackProps } from "react-error-boundary";
+
+export default function ErrorFallback({
+  error,
+  resetErrorBoundary,
+}: FallbackProps) {
+  return (
+    <div className="w-full h-full flex justify-center items-center flex-col gap-20">
+      <div className="flex flex-col gap-3 justify-center itmes-center">
+        <h1 className="text-5xl text-center">OOPS!</h1>
+        <h2>페이지를 로드하던 중 문제가 발생했어요</h2>
+      </div>
+      <Button className="w-3/4" onClick={resetErrorBoundary}>
+        다시 시도하기
+      </Button>
+    </div>
+  );
+}
