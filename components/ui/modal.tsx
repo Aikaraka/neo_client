2320e06ffcba0643d89236520da05020c76aa48c
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-29 18:38:28 +0900

    feat : 모달 관련 커스텀 훅과 컴포넌트 추가

---
+++ components/ui/modal.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+type ModalProps = {
+  children: React.ReactNode;
+  open: boolean;
+  switch: () => void;
+};
+
+export const Modal = ({ children, open, switch: toggle }: ModalProps) => {
+  if (!open) return null;
+
+  const handleBackgroundClick = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      toggle();
+    }
+  };
+
+  return (
+    <div
+      className="absolute top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 flex justify-center items-center z-40"
+      onClick={handleBackgroundClick}
+    >
+      <div className="bg-white p-6 rounded-lg shadow-lg w-3/4 max-w-[80%] z-50 flex flex-col gap-5">
+        {children}
+        <Button type="button" onClick={toggle}>
+          확인
+        </Button>
+      </div>
+    </div>
+  );
+};
