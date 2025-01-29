2133cd227fa2a22809b4abb27d070ccc12293438
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-29 18:45:08 +0900

    feat : signup 페이지 구조화

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,3 +1,20 @@
+import React from "react";
+import { PageProvider } from "@/components/ui/pageContext";
+import { ErrorBoundary } from "react-error-boundary";
+import { Toaster } from "@/components/ui/toaster";
+import SignupForm from "@/app/(auth)/signup/signupForm";
+import ErrorFallback from "@/components/common/errorFallback";
+
 export default function Signup() {
-  return <div>sign up</div>;
+  return (
+    <div className="h-screen relative bg-gray-100">
+      <Toaster>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <PageProvider maxPage={2}>
+            <SignupForm />
+          </PageProvider>
+        </ErrorBoundary>
+      </Toaster>
+    </div>
+  );
 }
