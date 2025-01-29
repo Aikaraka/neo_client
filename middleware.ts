89d8bfc415366f36ecb48802d64aba3517e2085d
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-30 03:32:55 +0900

    feat : 프로필 완성 안됐을시 모든 페이지 제한 추가

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
+import { createClient } from "@/utils/supabase/server";
 
 const protectedRoutes = ["/dashboard", "/profile", "/settings"];
 const authRoutes = ["/login", "/signup"];
@@ -7,31 +8,44 @@ const authRoutes = ["/login", "/signup"];
 export async function middleware(request: NextRequest) {
   // 세션 업데이트 처리
   const { supabaseResponse, user } = await updateSession(request);
-
+  const supabase = await createClient();
   const { pathname } = request.nextUrl;
 
-  if (user && authRoutes.includes(pathname)) {
-    const redirectUrl = new URL("/", request.url);
-    const redirectResponse = NextResponse.redirect(redirectUrl);
+  if (user) {
+    const { data: userData } = await supabase
+      .from("users")
+      .select()
+      .eq("id", user.id)
+      .single();
 
-    supabaseResponse.cookies.getAll().forEach((cookie) => {
-      redirectResponse.cookies.set(cookie.name, cookie.value, cookie);
-    });
+    if (!userData.profile_completed && pathname !== "/auth/setting") {
+      const profileSettingURL = new URL("/auth/setting", request.url);
+      return NextResponse.redirect(profileSettingURL);
+    }
 
-    return redirectResponse;
-  }
+    if (authRoutes.includes(pathname)) {
+      const redirectUrl = new URL("/", request.url);
+      const redirectResponse = NextResponse.redirect(redirectUrl);
 
-  if (!user && protectedRoutes.some((route) => pathname.startsWith(route))) {
-    const redirectUrl = new URL("/login", request.url);
-    redirectUrl.searchParams.set("returnUrl", pathname);
-    const redirectResponse = NextResponse.redirect(redirectUrl);
+      supabaseResponse.cookies.getAll().forEach((cookie) => {
+        redirectResponse.cookies.set(cookie.name, cookie.value, cookie);
+      });
 
-    // 쿠키 복사
-    supabaseResponse.cookies.getAll().forEach((cookie) => {
-      redirectResponse.cookies.set(cookie.name, cookie.value, cookie);
-    });
+      return redirectResponse;
+    }
+  } else {
+    if (protectedRoutes.some((route) => pathname.startsWith(route))) {
+      const redirectUrl = new URL("/login", request.url);
+      redirectUrl.searchParams.set("returnUrl", pathname);
+      const redirectResponse = NextResponse.redirect(redirectUrl);
 
-    return redirectResponse;
+      // 쿠키 복사
+      supabaseResponse.cookies.getAll().forEach((cookie) => {
+        redirectResponse.cookies.set(cookie.name, cookie.value, cookie);
+      });
+
+      return redirectResponse;
+    }
   }
 
   return supabaseResponse;
