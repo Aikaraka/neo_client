a8b173fa0b4a1a95780faad00a0d0535363c5781
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-29 18:39:03 +0900

    feat : 소셜 로그인 버튼 추가

--- components/ui/socialLoginButton.tsx
+++ components/ui/socialLoginButton.tsx
@@ -1,25 +1,29 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { signInWithOAuth } from "@/utils/supabase/client";
 import Image from "next/image";
 
 const loginType = {
   google: {
     name: "구글",
-    link: "",
     image: "/auth/google_emblem.svg",
   },
-  kakao: { name: "카카오", link: "", image: "/auth/kakao_emblem.svg" },
+  kakao: { name: "카카오", image: "/auth/kakao_emblem.svg" },
 } as const;
 
 type SocialLoginButtonType = {
   type: keyof typeof loginType;
 };
 export default function SocialLoginButton({ type }: SocialLoginButtonType) {
-  const { image, link, name } = loginType[type];
+  const { image, name } = loginType[type];
 
   return (
     <Button
       variant="outline"
       className="w-full p-6 rounded-lg text-base font-normal"
+      type="button"
+      onClick={() => signInWithOAuth(type)}
     >
       <Image src={image} alt={type} width={26} height={26} />
       {name} 로그인
