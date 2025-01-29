927ede7db096f60acb292213adaa89b1a14da7ba
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-29 18:48:20 +0900

    feat : 프로필 완성에 사용되는 비즈니스 로직 커스텀 훅으로 분리

---
+++ app/(auth)/auth/setting/hooks.ts
@@ -0,0 +1,49 @@
+"use client";
+
+import { initialProfileSubmit } from "@/app/(auth)/auth/setting/actions";
+import { settingFormSchema } from "@/app/(auth)/auth/setting/schema";
+import { useToast } from "@/hooks/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export function useSettingForm() {
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof settingFormSchema>>({
+    resolver: zodResolver(settingFormSchema),
+    defaultValues: {
+      birth: "20000101",
+      gender: "남성",
+      name: "",
+      nickname: "",
+    },
+    mode: "onChange",
+  });
+  const { mutate: submit, isPending } = useMutation({
+    mutationFn: (values: z.infer<typeof settingFormSchema>) => onSubmit(values),
+  });
+
+  async function onSubmit(values: z.infer<typeof settingFormSchema>) {
+    const { birth, gender, name, nickname } = values;
+    try {
+      await initialProfileSubmit(name, nickname, birth, gender);
+    } catch (error) {
+      if (error instanceof Error) {
+        toast({
+          title: "오류 안내",
+          description: error.message,
+        });
+      }
+      toast({
+        title: "오류 안내",
+        description: "서버 상의 오류가 발생했습니다.",
+      });
+    }
+  }
+  return {
+    form,
+    submit,
+    isPending,
+  };
+}
