a66d24fd202ab715d7ff14a7b3a6111a23b7abb3
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-29 18:40:00 +0900

    feat : 회원가입 관련 스키마 및 타입 추가

---
+++ app/(auth)/signup/schema.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const signupFormSchema = z
+  .object({
+    email: z.string().email({ message: "유효한 이메일 주소가 아닙니다." }),
+    password: z
+      .string()
+      .regex(
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        {
+          message:
+            "비밀번호는 영문 대/소문자, 숫자, 특수문자를 포함하여 8자 이상이어야 합니다.",
+        }
+      ),
+    passwordConfirm: z.string().nonempty({
+      message: "비밀번호 확인을 입력해주세요.",
+    }),
+    // name: z
+    //   .string()
+    //   .regex(/^[가-힣]{2,6}$/, { message: "이름을 제대로 입력해주세요." }),
+    terms: z.boolean({ required_error: "약관에 동의해야 가입이 가능합니다." }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "비밀번호와 비밀번호 확인이 일치하지 않습니다.",
+    path: ["passwordConfirm"],
+  });
+
+export type SignupFormType = z.infer<typeof signupFormSchema>;
+export type SignupFormFieldName = keyof SignupFormType;
