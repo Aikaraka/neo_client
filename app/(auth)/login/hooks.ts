b32eb68d4e285daa10b5b47c1e69488e1907fed9
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-29 18:49:13 +0900

    feat : 로그인 관련 비즈니스 로직 커스텀 훅으로 분리

---
+++ app/(auth)/login/hooks.ts
@@ -0,0 +1,61 @@
+"use client";
+
+import { handleEmailLogin } from "@/app/(auth)/login/login.api";
+import {
+  loginFormSchema,
+  loginFormSchemaType,
+} from "@/app/(auth)/login/schema";
+import useModal from "@/hooks/use-modal";
+import { useAuth } from "@/utils/supabase/authProvider";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+export function useLogin() {
+  const { supabase } = useAuth();
+  const router = useRouter();
+  const {
+    open: openErrorModal,
+    switchModal,
+    setMessage: setErrorMessage,
+    message: errorMessage,
+  } = useModal();
+
+  const form = useForm<loginFormSchemaType>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const { mutate, isPending } = useMutation({
+    mutationFn: handleEmailLogin,
+    onSuccess: () => router.push("/"),
+    onError: (error) => {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+        switchModal();
+      } else {
+        setErrorMessage(
+          "서버 상의 이유로 실패하였습니다.\n 잠시 후 다시 시도해 주세요."
+        );
+        switchModal();
+      }
+    },
+  });
+
+  function submit(values: loginFormSchemaType) {
+    const { email, password } = values;
+    mutate({ email, password, supabase });
+  }
+  return {
+    form,
+    submit,
+    openErrorModal,
+    switchModal,
+    setErrorMessage,
+    isPending,
+    errorMessage,
+  };
+}
