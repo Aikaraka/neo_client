507f82a508b2af50ee3a391e21fead67e3acedb4
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-03 23:15:30 +0900

    refactor : 모달 성격에 따라 커스터마이징이 가능하도록 개선

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -5,13 +5,21 @@ type ModalProps = {
   children: React.ReactNode;
   open: boolean;
   switch: () => void;
+  onConfirm?: () => void;
+  confirmText?: string;
 };
 
-export const Modal = ({ children, open, switch: toggle }: ModalProps) => {
+export const Modal = ({
+  children,
+  open,
+  switch: toggle,
+  onConfirm,
+  confirmText,
+}: ModalProps) => {
   if (!open) return null;
 
   const handleBackgroundClick = (e: React.MouseEvent) => {
-    if (e.target === e.currentTarget) {
+    if (e.target === e.currentTarget && !onConfirm) {
       toggle();
     }
   };
@@ -23,8 +31,8 @@ export const Modal = ({ children, open, switch: toggle }: ModalProps) => {
     >
       <div className="bg-white p-6 rounded-lg shadow-lg w-3/4 max-w-[80%] z-50 flex flex-col gap-5">
         {children}
-        <Button type="button" onClick={toggle}>
-          확인
+        <Button type="button" onClick={onConfirm ?? toggle}>
+          {confirmText ?? "확인"}
         </Button>
       </div>
     </div>
