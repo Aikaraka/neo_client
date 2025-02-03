a20c64f4379b6d1fe5ebba3112f1c718a117157d
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-03 23:16:13 +0900

    feat : 계정 찾기 관련 supabase server actions 추가

--- utils/supabase/service/auth.ts
+++ utils/supabase/service/auth.ts
@@ -1,8 +1,58 @@
-"use client";
+"use server";
 
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "@/utils/supabase/server";
 
-export const signout = () => {
-  const supabse = createClient();
-  return supabse.auth.signOut();
+export const signout = async () => {
+  const supabase = await createClient();
+  return supabase.auth.signOut();
+};
+
+export const resetPassword = async ({
+  email,
+  name,
+}: {
+  email: string;
+  name: string;
+}) => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("email", email);
+
+  if (error) throw new Error("이메일에 해당하는 계정을 찾을 수 없습니다.");
+
+  const userName = data[0].name;
+
+  if (name !== userName) throw new Error("정보가 일치하지 않습니다.");
+
+  // TODO: 환경변수로 변경
+  return supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: "http://localhost:3000/auth/callback/update-password",
+  });
+};
+
+export const changePassword = async ({ password }: { password: string }) => {
+  const supabase = await createClient();
+  return await supabase.auth.updateUser({ password: password });
+};
+
+export const findEmail = async ({
+  name,
+  birth,
+}: {
+  name: string;
+  birth: string;
+}): Promise<string> => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("name", name)
+    .eq("birthdate", birth);
+  if (!data?.length || error) {
+    throw new Error("정보와 해당하는 이메일이 없습니다.");
+  }
+
+  return data[0].email;
 };
