90d2b1a79773fa919440a89c8b0f7a7dfdad911b
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-22 02:42:47 +0900

    feat : 소설 생성하는 서버 함수 추가

---
+++ app/create/_api/createNovel.server.ts
@@ -0,0 +1,39 @@
+"use server";
+import { createNovelSchema } from "@/app/create/_schema/createNovelSchema";
+import { createClient } from "@/utils/supabase/server";
+import { z } from "zod";
+
+export async function createNovel(novel: z.infer<typeof createNovelSchema>) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+  if (!user || userError) throw new Error("유저 정보를 찾지 못했습니다.");
+
+  const { data, error: insertError } = await supabase
+    .from("novels")
+    .insert([
+      {
+        user_id: user.id,
+        title: novel.title,
+        image_url: novel.cover_image_url ?? "",
+        settings: novel.settings,
+        characters: novel.characters || "{}",
+        plot: novel.plot || "",
+        background: {
+          description: novel.background.description || "",
+          detailedLocations: novel.background.detailedLocations || "{}",
+        },
+        ending: novel.ending || "happy",
+        mood: novel.mood || "{}",
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+      },
+    ])
+    .select()
+    .single();
+
+  if (insertError) throw new Error("소설 업로드 중 오류가 발생했습니다.");
+  return data.id;
+}
