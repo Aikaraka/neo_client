47096dec4c97274660f9eca25132c0cf471b31c9
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-23 12:27:10 +0900

    feat : 추천 소설과 인기 소설 api 추가

---
+++ utils/supabase/service/novel.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+
+export async function getRecommendedNovels() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("novels").select("*").limit(8);
+  if (error) throw new Error("소설 정보를 가져오던 중 오류가 발생했습니다.");
+  return data;
+}
+
+export async function getTopNovels() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("novels").select("*").limit(8);
+  if (error) throw new Error("소설 정보를 가져오던 중 오류가 발생했습니다.");
+  return data;
+}
