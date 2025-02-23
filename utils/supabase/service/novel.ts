9cbcd55e0eb4c0dd57e5397d8f4c67c2373823ec
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-24 02:30:29 +0900

    feat : 소설 삭제 서버함수 추가

--- utils/supabase/service/novel.ts
+++ utils/supabase/service/novel.ts
@@ -15,3 +15,12 @@ export async function getTopNovels() {
   if (error) throw new Error("소설 정보를 가져오던 중 오류가 발생했습니다.");
   return data;
 }
+
+export async function deleteNovel(novelId: string) {
+  const supabase = await createClient();
+  const { error } = await supabase
+    .from("novels")
+    .delete({ count: "exact" })
+    .eq("id", novelId);
+  if (error) throw new Error("소설 삭제 중 오류가 발생했습니다.");
+}
