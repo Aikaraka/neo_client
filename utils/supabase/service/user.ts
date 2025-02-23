918a07ad91e2d8cc8b2e1f70354c9ec3a7208964
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-23 12:25:14 +0900

    feat : 유저 정보 가져오기 api 추가

--- utils/supabase/service/user.ts
+++ utils/supabase/service/user.ts
@@ -11,6 +11,47 @@ export async function getCurrentUser() {
     .select("*")
     .eq("id", data.user.id)
     .single();
-  if (notFoundError) throw new Error("유저 정보를 찾을 수 없습니다.");
-  return userData;
+  if (notFoundError) {
+    throw new Error("유저 정보를 찾을 수 없습니다.");
+  }
+  const { data: tokenData, error: tokenError } = await supabase
+    .from("user_ai_token")
+    .select("*")
+    .eq("user_id", data.user.id)
+    .single();
+  if (tokenError) throw new Error("유저 정보를 찾을 수 없습니다.");
+
+  const { data: novelData, error: novelError } = await supabase
+    .from("novels")
+    .select("*")
+    .eq("user_id", data.user.id);
+  if (novelError) throw new Error("소설 정보를 가져오지 못했습니다.");
+  const result = { user: userData, token: tokenData, novels: novelData };
+  console.log(result);
+  return result;
+}
+
+export async function getLastViewedNovels() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (error) throw new Error("유저 정보를 가져오지 못했습니다.");
+  const { data: novels, error: novelNotFound } = await supabase
+    .from("novel_views")
+    .select(
+      `
+      novel_id,
+      last_viewed_at,
+      novels (
+        id,
+        title,
+        image_url,
+        mood,
+        created_at
+      )
+    `
+    )
+    .eq("user_id", data.user.id)
+    .order("last_viewed_at", { ascending: false });
+  if (novelNotFound) throw new Error("유저 정보를 찾을 수 없습니다.");
+  return novels;
 }
