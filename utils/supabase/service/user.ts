565e0518b644581dd12c33488dc1c69dde464709
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-24 02:30:57 +0900

    feat : 로그인 상태 가져오는 서버 함수 추가

--- utils/supabase/service/user.ts
+++ utils/supabase/service/user.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
+import { AuthError } from "@supabase/supabase-js";
 
 export async function getCurrentUser() {
   const supabase = await createClient();
@@ -27,7 +28,6 @@ export async function getCurrentUser() {
     .eq("user_id", data.user.id);
   if (novelError) throw new Error("소설 정보를 가져오지 못했습니다.");
   const result = { user: userData, token: tokenData, novels: novelData };
-  console.log(result);
   return result;
 }
 
@@ -54,4 +54,14 @@ export async function getLastViewedNovels() {
     .order("last_viewed_at", { ascending: false });
   if (novelNotFound) throw new Error("유저 정보를 찾을 수 없습니다.");
   return novels;
+}
+
+export async function getLoginState() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+  if (error || !user) throw new AuthError("유저 정보를 찾을 수 없습니다.");
+  return;
 }
