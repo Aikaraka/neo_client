1366eef92944a3dfea7702582fdbaf45f7a39587
Minhyung Cho <dup33333@hufs.ac.kr>
2025-03-02 00:30:01 +0900

    feat : 마이페이지 링크 추가

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { getCurrentUser } from "@/app/mypage/_api/currentUser.server";
 import { Plus, User } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Page() {
   const userData = await getCurrentUser();
@@ -50,32 +51,36 @@ export default async function Page() {
         </div>
       </section>
       <section className="py-5">
-        <Button className="w-full p-6 [&_svg]:size-5" variant={"outline"}>
-          <Plus className="text-primary" /> 소설 제작
-        </Button>
+        <Link href={"/create"}>
+          <Button className="w-full p-6 [&_svg]:size-5" variant={"outline"}>
+            <Plus className="text-primary" /> 소설 제작
+          </Button>
+        </Link>
       </section>
       <section>
         <p className="mb-5">내 소설</p>
         <div className="w-full grid grid-cols-3 place-items-center pb-5 gap-x-2 gap-y-4">
           {userData.novels.map((novel, index) => (
-            <Card key={index} className="w-[100px] shrink-0">
-              <CardContent className="p-0">
-                <Image
-                  src={
-                    novel.image_url
-                      ? novel.image_url
-                      : "/src/public/neo_emblem.svg"
-                  }
-                  alt={novel.title}
-                  width={150}
-                  height={150}
-                  className="rounded-t-lg object-cover w-[100px] h-[100px]"
-                />
-                <div className="p-2">
-                  <p className="text-sm truncate">{novel.title}</p>
-                </div>
-              </CardContent>
-            </Card>
+            <Link href={`/novel/${novel.id}/detail`}>
+              <Card key={index} className="w-[100px] shrink-0">
+                <CardContent className="p-0">
+                  <Image
+                    src={
+                      novel.image_url
+                        ? novel.image_url
+                        : "/src/public/neo_emblem.svg"
+                    }
+                    alt={novel.title}
+                    width={150}
+                    height={150}
+                    className="rounded-t-lg object-cover w-[100px] h-[100px]"
+                  />
+                  <div className="p-2">
+                    <p className="text-sm truncate">{novel.title}</p>
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
           ))}
         </div>
       </section>
