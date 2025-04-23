8680a4e2609becfd3ce71dcfe1a5e47386c2cf12
Minhyung Cho <dup33333@hufs.ac.kr>
2025-04-23 15:40:10 +0900

    feat : Header 컴포넌트에 아이콘 및 자식 요소 추가

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -5,18 +5,26 @@ import { HtmlHTMLAttributes } from "react";
 export default function Header({
   prevPageButton = false,
   title,
+  icon,
+  children,
   ...props
 }: HtmlHTMLAttributes<HTMLHeadElement> & {
+  children?: React.ReactNode;
   prevPageButton: boolean;
   title?: string;
+  icon?: React.ReactNode;
 }) {
   return (
     <header
       {...props}
-      className={cn("p-6 text-xl text-center", props.className)}
+      className={cn("p-6 text-xl text-center w-full relative", props.className)}
     >
-      {prevPageButton && <PrevPageButton />}
-      {title}
+      <div className="flex gap-3 items-center justify-center">
+        {icon}
+        {title}
+      </div>
+      {prevPageButton && <PrevPageButton className="absolute left-0 top-4" />}
+      {children}
     </header>
   );
 }
