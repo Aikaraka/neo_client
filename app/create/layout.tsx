fed0c33780ae67f8bd428500b6b744b590ad05b0
Minhyung Cho <dup33333@hufs.ac.kr>
2025-04-24 22:03:49 +0900

    feat : CreateLayout 컴포넌트에 글꼴 로드 및 불필요한 코드 제거

--- app/create/layout.tsx
+++ app/create/layout.tsx
@@ -1,7 +1,18 @@
 import Navbar from "@/components/layout/navbar";
+import { MainContent } from "@/components/ui/content";
 import { PageProvider } from "@/components/ui/pageContext";
 import { Toaster } from "@/components/ui/toaster";
-import { PencilLine } from "@/public/navbar";
+import localFont from "next/font/local";
+
+const heiroOfLight = localFont({
+  src: "../fonts/HeirofLightRegular.ttf",
+  display: "swap",
+});
+
+const bombaram = localFont({
+  src: "../fonts/bombaram.ttf",
+  display: "swap",
+});
 
 export default function CreateLayout({
   children,
@@ -10,18 +21,12 @@ export default function CreateLayout({
 }>) {
   return (
     <Toaster>
-      <main className="w-full h-svh pb-20  flex flex-col">
-        <header className="text-center text-xl p-4 flex items-center justify-center gap-2 font-semibold relative">
-          <div className="flex gap-2 items-center">
-            <PencilLine />
-            <p>나만의 소설 만들기</p>
-          </div>
-        </header>
-        <div className="overflow-auto">
+      <main className="w-full h-full pb-20 md:pb-0 flex flex-col md:bg-input justify-center items-center">
+        <MainContent className="relative">
           <PageProvider maxPage={2} variants={{ variant: "header" }}>
             {children}
           </PageProvider>
-        </div>
+        </MainContent>
         <Navbar />
       </main>
     </Toaster>
