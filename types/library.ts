db3ed4fa1466debe41c8f2f9747b402fa97b71f7
jaminitachi <limdehan@gmail.com>
2025-04-26 17:02:20 +0000

    Goodbye Minhyoung!

--- types/library.ts
+++ types/library.ts
@@ -1,9 +1,7 @@
-interface LibraryNovel {
+export interface LibraryNovel {
   novel_id: string;
   title: string;
   cover_image: string | null;
   progress_rate: number;
   last_viewed_at: string | null;
-}
-
-export type { LibraryNovel };
+} 
