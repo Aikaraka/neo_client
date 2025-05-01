0904b570ca3bcf710291c25a964f515c8386fec1
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-01 22:05:54 +0900

    type : LibraryNovel 인터페이스 추가

--- types/library.ts
+++ types/library.ts
@@ -1,7 +1,9 @@
-export interface LibraryNovel {
+interface LibraryNovel {
   novel_id: string;
   title: string;
   cover_image: string | null;
   progress_rate: number;
   last_viewed_at: string | null;
-} 
+}
+
+export type { LibraryNovel };
