406118534ee7352c6e0907c0db55f69ac2dd228c
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-01 22:04:38 +0900

    feat : Book 디자인 공통 컴포넌트 추가 및 variant 추가

--- components/ui/book.tsx
+++ components/ui/book.tsx
@@ -5,38 +5,64 @@ import Image from "next/image";
 import Link from "next/link";
 import { ScrollArea, ScrollBar } from "@/components/layout/scroll-area";
 import { twMerge } from "tailwind-merge";
+import { cva, VariantProps } from "class-variance-authority";
 
-const Book = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & { href: string }
->(({ className, href, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "bg-card text-card-foreground shadow-sm md:w-[210px] md:h-[270px] shrink-0 relative w-[105px] h-[140px]",
-      className
-    )}
-    {...props}
-  >
-    <Link href={href}>
-      {props.children}
-      <Image
-        src={"/novel/book_template.png"}
-        width={180}
-        height={240}
-        className="absolute h-full top-0 z-20"
-        alt="book_template"
-      />
-      <Image
-        src={"/novel/book_shadow.png"}
-        width={119}
-        height={59}
-        alt="book_shadow"
-        className="absolute bottom-0 right-5 z-10"
-      />
-    </Link>
-  </div>
-));
+type BookVariantProps = VariantProps<typeof bookVariants>;
+const bookVariants = cva("", {
+  variants: {
+    size: {
+      default: "md:w-[210px] md:h-[270px] w-[105px] h-[140px]",
+      extraSmall: "w-[45px] h-[60px]",
+      small: "w-[105px] h-[140px]",
+      medium: "w-[126px] h-[168px]",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+interface BookProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    BookVariantProps {
+  href: string;
+  shadow?: boolean;
+}
+const Book = React.forwardRef<HTMLDivElement, BookProps>(
+  ({ className, size, href, shadow = true, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "bg-card text-card-foreground shadow-sm shrink-0 relative z-10",
+        className,
+        bookVariants({ size })
+      )}
+      {...props}
+    >
+      <Link href={href}>
+        {props.children}
+        <Image
+          src={"/novel/book_template.png"}
+          width={180}
+          height={240}
+          className={cn("absolute top-0 z-10", bookVariants({ size }))}
+          alt="book_template"
+        />
+        <Image
+          src={"/novel/book_shadow.png"}
+          width={119}
+          height={59}
+          alt="book_shadow"
+          className={cn(
+            "absolute bottom-0 right-5 -z-10",
+            bookVariants({ size }),
+            !shadow && "hidden"
+          )}
+        />
+      </Link>
+    </div>
+  )
+);
 Book.displayName = "Book";
 
 const BookShelf = React.forwardRef<
@@ -48,10 +74,18 @@ const BookShelf = React.forwardRef<
       {props.children}
     </div>
     <ScrollBar orientation="horizontal" />
-    <div className="h-1/2 w-full absolute bottom-5 bg-[#dbdbdb]" />
-    <div className="h-5 bg-[#F6F3F1] shadow-bookshelf " />
+    <div className="h-1/2 w-full absolute bottom-5 z-[5] bg-[#dbdbdb]" />
+    <div className="h-5 bg-[#F6F3F1] shadow-bookshelf" />
   </ScrollArea>
 ));
 BookShelf.displayName = "BookShelf";
 
-export { Book, BookShelf };
+const BookContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("p-6 pt-0 z-[25]", className)} {...props} />
+));
+BookContent.displayName = "BookContent";
+
+export { Book, BookShelf, BookContent };
