c2ccaca46d18f17b32ca6730cc2a91a112ddf602
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-01 22:03:04 +0900

    feat : NovelListView 컴포넌트에서 데이터 타입 수정 및 레이아웃 개선

--- app/library/_components/novelListView.tsx
+++ app/library/_components/novelListView.tsx
@@ -1,39 +1,46 @@
 import { Book } from "@/components/ui/book";
 import { Button } from "@/components/ui/button";
-import { Tables } from "@/utils/supabase/types/database.types";
+import { Progress } from "@/components/ui/progress";
+import { LibraryNovel } from "@/types/library";
 import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 
 function NovelListView({
   novelList,
 }: {
-  novelList: Tables<"novels">[] | undefined;
+  novelList: LibraryNovel[] | undefined;
 }) {
   return (
     <>
       {novelList?.map((novel) => (
         <div
-          key={novel.id}
-          className="p-2 border-b hover:shadow-md transition-shadow cursor-pointer flex gap-2 last:border-none items-center"
+          key={novel.novel_id}
+          className="p-2  hover:shadow-md transition-shadow cursor-pointer flex gap-2 items-center"
         >
-          <Book href="" className="w-[45px] h-[60px] relative">
-            {novel.image_url ? (
+          <Book
+            href={`/novel/${novel.novel_id}/chat`}
+            className="relative"
+            size={"extraSmall"}
+            shadow={false}
+          >
+            {novel.cover_image ? (
               <Image
-                src={novel.image_url}
+                src={novel.cover_image}
                 alt={novel.title}
                 width={60}
                 height={60}
-                className="rounded-lg object-contain w-[60px] h-[60px] "
+                className="object-cover w-[45px] h-[60px]"
               />
             ) : (
-              <div className=" bg-gray-200 rounded-lg w-[60px] h-[60px]" />
+              <div className=" bg-gray-200 w-[45px] h-[60px]" />
             )}
           </Book>
           <div className="flex flex-col flex-1 justify-center">
             <p className="text-lg font-semibold">{novel.title}</p>
-            <p className="text-xs text-gray-600 mb-2">
-              {novel.created_at.substring(0, 10)}
-            </p>
+            <div className="flex items-center justify-between gap-2">
+              <Progress className="h-3 w-full" value={novel.progress_rate} />
+              <span>{novel.progress_rate}%</span>
+            </div>
           </div>
           <Button variant={"ghost"} className="hover:bg-none">
             이어보기
