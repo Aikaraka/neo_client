3495614934c7c2197da5fe6c463d3184ca3adb11
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-03 03:07:05 +0900

    fix: createClient 함수에 인증 옵션 추가

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -10,7 +10,13 @@ if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 export const createClient = () => {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+      },
+    }
   );
 };
 
